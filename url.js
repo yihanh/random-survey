530e29f80db3140a26745bbae67fde97294a0dda
yihanh <yibelief@gmail.com>
2025-03-25 23:35:34 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,6 +9,12 @@
 
 const urls = [
 	
-	'https://www.surveycake.com/s/nz09M',
-	'https://www.surveycake.com/s/xKbGy'
+	'https://www.surveycake.com/s/Yo9K7',
+	'https://www.surveycake.com/s/2MkyV',
+	'https://www.surveycake.com/s/pMbNZ',
+	'https://www.surveycake.com/s/1Zq0l',
+	'https://www.surveycake.com/s/gArZa',
+	'https://www.surveycake.com/s/Pg3Bm',
+	'https://www.surveycake.com/s/R6Q3e',
+	'https://www.surveycake.com/s/eyw44'
 ];
