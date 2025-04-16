0fdc8b1dda26f2c27a742fd4f3b4143f332532bf
yihanh <yibelief@gmail.com>
2025-04-16 09:13:40 +0800

    Update url.js

--- url.js
+++ url.js
@@ -9,7 +9,9 @@
 
 const urls = [
 		
-	'https://www.surveycake.com/s/2MkyV',
+	'https://www.surveycake.com/s/Pg3Bm',
+	'https://www.surveycake.com/s/1Zq0l',
+	'https://www.surveycake.com/s/eyw44',
 	'https://www.surveycake.com/s/Yo9K7',
-	'https://www.surveycake.com/s/eyw44'
+	'https://www.surveycake.com/s/2MkyV'
 ];
